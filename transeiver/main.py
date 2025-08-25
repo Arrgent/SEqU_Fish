4798c0ea6eaed46a104d5cbd2ba099e70f76e38a
Arrgent <128487508+Arrgent@users.noreply.github.com>
2025-08-25 16:44:47 +0300

    Add files via upload

--- transeiver/main.py
+++ transeiver/main.py
@@ -13,9 +13,39 @@ pinnp=Pin(33,Pin.OUT)
 np=NeoPixel(pinnp,1)
 np[0]=(10,10,10)
 np.write()
+#---------------------------
+a=False 
+#===================================================
+def iraq1(pin):
+   e.send(peer,"pin1")
+    
+   print("pin1") 
+#-------------------------
+def iraq2(pin):
+   global a 
+   e.send(peer,"pin1")
+   a=not a
+       
+   if a:        
+     e.send(peer,"pinon")
+     print(a)
+   if a==False:
+     e.send(peer,"pinoff") 
+   print("pin2")
+#--------------------------   
+def iraq3(pin):
+   
+     e.send(peer,"pin3")
+    
+     print("pin3")
+
+#==================================================
 pin1=Pin(34,Pin.IN,Pin.PULL_UP)
+pin1.irq(handler=iraq1,trigger=Pin.IRQ_FALLING)
 pin2=Pin(35,Pin.IN,Pin.PULL_UP)
+pin2.irq(handler=iraq2,trigger=Pin.IRQ_FALLING)
 pin3=Pin(32,Pin.IN,Pin.PULL_UP)
+pin3.irq(handler=iraq3,trigger=Pin.IRQ_FALLING)
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.WLAN.IF_STA)  # Or network.WLAN.IF_AP
 sta.active(True)
@@ -27,7 +57,7 @@ e.active(True)
 peer = b'\x10\x00;Pz\xc8'   # MAC address of peer's wifi interface
 e.add_peer(peer)      # Must add_peer() before send()
 e.send(peer, "Starting...")
-a=False 
+
 while True :
    host, msg = e.recv(5)
    if msg:
@@ -38,28 +68,11 @@ while True :
        if msg==b'signon':
            np[0]=(0,250,0)
            np.write()
+           print(msg)
        if msg==b'signoff':
            np[0]=(0,0,25)
            np.write()    
 #================transeiver===================           
-   if(pin1.value()==0):
-      e.send(peer,"pin1")
-    
-      print("pin1")
-   #------------------------------  
-   if(pin2.value()==0):
-       a=not a
-       
-       if a:        
-          e.send(peer,"pinon")
-          print(a)
-       if a==False:
-          e.send(peer,"pinoff")
-          print(a)
-   #-------------------------------       
-   if(pin3.value()==0):
-      e.send(peer,"pin3")
-     
    time.sleep(0.1)
-   
+      #print("pin1")   
 #e.send(peer, b'end')
